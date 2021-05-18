7c355d702e510e735eaa13f7d4317fbdfd354304
Bri Davidson <brynnd6@gmail.com>
2021-05-17 18:43:02 -0600

    2.07 push

---
+++ src/objectsAndArrays.ts
@@ -0,0 +1,17 @@
+var myPerson = {
+    firstName: "Brianne";
+    lastName: "Davidson";
+    age: 25
+};
+console.log(`Person:\n\t Name: " ${myPerson.firstName"} ${myperson.lastName}" \n\t Age: ${myperson.age}`
+);
+console.log("\nOur array\n==========");
+
+var icecreamFlacors: Array<string> = [
+    "Strawberry";
+    "Cheesecake";
+    "Chocolate";
+    "Vanilla";
+    "Butter Pecan";
+
+]
